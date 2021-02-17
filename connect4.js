d635c1abc5c952269c3b1f1b86f22b69fbfebf60
mits <dmitrybaycharov@gmail.com>
2021-02-17 17:36:35 -0500

    fixed board being upside down

--- connect4.js
+++ connect4.js
@@ -35,17 +35,7 @@ function makeBoard() {
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.querySelector('#board');
-  // creates the top row of clickable/playable locations/columns
-  const top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
 
-  for (let x = 0; x < WIDTH; x++) {
-    const headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
-    top.append(headCell);
-  }
-  htmlBoard.append(top);
 
   // creates the 'field' 
   for (let y = 0; y < HEIGHT; y++) {
@@ -55,8 +45,21 @@ function makeHtmlBoard() {
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
     }
-    htmlBoard.append(row);
+    htmlBoard.prepend(row);
   }
+
+    // creates the top row of clickable/playable locations/columns
+    const top = document.createElement("tr");
+    top.setAttribute("id", "column-top");
+    top.addEventListener("click", handleClick);
+  
+    for (let x = 0; x < WIDTH; x++) {
+      const headCell = document.createElement("td");
+      headCell.setAttribute("id", x);
+      top.append(headCell);
+    }
+    htmlBoard.prepend(top);
+
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
